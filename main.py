e0c63cdf35ecc5f79287eff1d463a29301143629
powdernails <97328479+powdernails@users.noreply.github.com>
2026-02-23 23:04:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
+import requests
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+owpe= "https://api.openweathermap.org/data/2.5/forecast"
+LAT = 30.319781200140344
+LONG = -97.85182214531893
+cnt = 4
+api_key = os.environ.get("OWM_API_KEY")
+my_email = os.environ.get("EMAIL_US")
+password = os.environ.get("PASSWORD")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+parameters = {
+    "lat": LAT,
+    "lon": LONG,
+    "appid": api_key,
+    "cnt": cnt
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+}
+response = requests.get(url=owpe, params= parameters)
+response.raise_for_status()
+weather_data = response.json()
+print (weather_data)
+for i in range(cnt):
+    if weather_data["list"][i]["weather"][0]["id"] < 700:
+        will_rain = True
+    else:
+        will_rain = False
+print (will_rain)
+if will_rain:
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="kiran.gonsalves@gmail.com",
+                        msg=f"Subject:weather condition\n\nDear Valued Citizen,\n\n"
+                            f"Please bring an umbrella today\n\n"
+                            f"Love,\n"
+                            f"Sanchia")
+    connection.close()
